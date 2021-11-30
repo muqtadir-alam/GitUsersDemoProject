5299fb28dd9e0a9a235f754a7962fbd7c9230ca6
muqtadir alam <alammit2015@gmail.com>
2021-12-01 00:23:11 +0530

    code updated

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -16,102 +16,112 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 0,
-    paddingTop: '56.25%', // 16:9
-  },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
-  },
-  avatar: {
-    backgroundColor: red[500],
-  },
+import { Modal, Button } from 'react-bootstrap';
+import { ShareModal } from '../Components/ShareModal';
+const useStyles = makeStyles(theme => ({
+	root: {
+		maxWidth: 345,
+	},
+	media: {
+		height: 0,
+		paddingTop: '56.25%', // 16:9
+	},
+	expand: {
+		transform: 'rotate(0deg)',
+		marginLeft: 'auto',
+		transition: theme.transitions.create('transform', {
+			duration: theme.transitions.duration.shortest,
+		}),
+	},
+	expandOpen: {
+		transform: 'rotate(180deg)',
+	},
+	avatar: {
+		backgroundColor: red[500],
+	},
 }));
 
 export default function RecipeReviewCard(props) {
-    const { name, img, id, followers ,url} = props;
-   
-  const classes = useStyles();
-  const [expanded, setExpanded] = React.useState(false);
+	const { name, img, id, followers, url } = props;
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
+	const classes = useStyles();
+	const [expanded, setExpanded] = React.useState(false);
+	const [modalShow, setModalShow] = React.useState(false);
+	const [Like, setLike] = React.useState(false);
 
-  return (
-    <Card className={classes.root}>
-      <CardHeader
-        avatar={
-          <Avatar aria-label="recipe" className={classes.avatar}>
-            {name[0]}
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title={name}
-        subheader="github user"
-        
-      />
-      <CardMedia
-        className={classes.media}
-        image={img}
-        title={name}
-      />
-      <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-        git rank {id}
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-                  <FavoriteIcon style={ {color:"red"}}/>
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon  style={ {color:"green"}} />
-        </IconButton>
-        <IconButton
-          className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded,
-          })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </IconButton>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-           i am on git hub
-          </Typography>
-          <Typography paragraph>
-           if u like me follow me on github...
-          </Typography>
-          <Typography paragraph>
-          i am here to help u...
-          </Typography>
-          <Typography>
-        follow the link {followers } <br/> {url}.
-          </Typography>
-        </CardContent>
-      </Collapse>
-    </Card>
-  );
+	const handleExpandClick = () => {
+		setExpanded(!expanded);
+	};
+	const LikeHandler = () => {
+		console.log('this is ');
+	};
+
+	return (
+		<>
+			<Card className={classes.root}>
+				<CardHeader
+					avatar={
+						<Avatar aria-label='recipe' className={classes.avatar}>
+							{name[0]}
+						</Avatar>
+					}
+					action={
+						<IconButton aria-label='settings'>
+							<MoreVertIcon />
+						</IconButton>
+					}
+					title={name}
+					subheader='github user'
+				/>
+				<CardMedia className={classes.media} image={img} title={name} />
+				<CardContent>
+					<Typography variant='body2' color='textSecondary' component='p'>
+						git rank {id}
+					</Typography>
+				</CardContent>
+				<CardActions disableSpacing>
+					<IconButton
+						aria-label='add to favorites'
+						onClick={() => setLike(true)}>
+						<FavoriteIcon
+							style={{ color: Like === true ? 'red' : '' }}
+							onClick={LikeHandler}
+						/>
+					</IconButton>
+					<IconButton aria-label='share'>
+						<ShareIcon
+							style={{ color: 'green' }}
+							onClick={() => setModalShow(true)}
+						/>
+					</IconButton>
+					<IconButton
+						className={clsx(classes.expand, {
+							[classes.expandOpen]: expanded,
+						})}
+						onClick={handleExpandClick}
+						aria-expanded={expanded}
+						aria-label='show more'>
+						<ExpandMoreIcon />
+					</IconButton>
+				</CardActions>
+				<Collapse in={expanded} timeout='auto' unmountOnExit>
+					<CardContent>
+						{/* <Typography paragraph>Method:</Typography> */}
+						<Typography paragraph>i am on git hub</Typography>
+						<Typography paragraph>
+							if u like me follow me on github...
+						</Typography>
+						<Typography paragraph>i am here to help u...</Typography>
+						<Typography>
+							follow the link {followers} <br /> {url}.
+						</Typography>
+					</CardContent>
+				</Collapse>
+			</Card>
+			<ShareModal
+				show={modalShow}
+				onHide={() => setModalShow(false)}
+				url={url}></ShareModal>
+		</>
+	);
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,30 +6,34 @@ import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
+const useStyles = makeStyles(theme => ({
+	root: {
+		flexGrow: 1,
+	},
+	menuButton: {
+		marginRight: theme.spacing(2),
+	},
 }));
 
 export default function DenseAppBar() {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  return (
-    <div className={classes.root}>
-      <AppBar position="fixed"  style={{backgroundColor:"green"}}>
-        <Toolbar variant="dense">
-          <IconButton edge="start" className={classes.menuButton} color="green" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="green">
-            git users
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+	return (
+		<div className={classes.root}>
+			<AppBar position='fixed' style={{ backgroundColor: 'green' }}>
+				<Toolbar variant='dense'>
+					<IconButton
+						edge='start'
+						className={classes.menuButton}
+						color='green'
+						aria-label='menu'>
+						<MenuIcon />
+					</IconButton>
+					<Typography variant='h6' color='green'>
+						git users
+					</Typography>
+				</Toolbar>
+			</AppBar>
+		</div>
+	);
 }
